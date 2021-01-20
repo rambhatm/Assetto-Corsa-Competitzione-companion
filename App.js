6eafd66b12bb0865776c1f4335243f529186591c
rambhatm <rammulky@gmail.com>
2021-01-20 08:07:13 +0530

    initial

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { DefaultTheme, Provider as PaperProvider, Appbar } from 'react-native-paper'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <PaperProvider theme={theme}>
+            <Appbar></Appbar>
+            <View>
+                <Text>Assetto Corsa Competetizione Scheduling app+!</Text>
+                <StatusBar style="auto" />
+            </View>
+        </PaperProvider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
+
+const theme = {
+    //  ...DefaultTheme,
+    dark: true,
+    mode: 'exact',
+    colors: {
+        primary: '#BB86FC',
+        accent: '#03DAC5',
+        background: '#121212',
+        surface: '#121212'
+    }
+}
